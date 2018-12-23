0025f10240467195c6860f11f01e6e95def7a27f
Josh Pollmann <joshpollmann102@gmail.com>
2018-12-23 13:16:37 -0500

    Changed some predictions to use scores to predict winner instead

--- nflPredict.py
+++ nflPredict.py
@@ -31,6 +31,7 @@ Okeys = ['Y/P','TO','PYds','PTD','Int','RYds','RTD','OPenYds','Sc%','OTO%']
 Dkeys = ['OY/P','FTO','OPYds','OPTD','FInt','ORYds','ORTD','DPenYds','OSc%','DTO%']
 Wkeys = ['WY/P','WTO','WPYds','WPTD','WInt','WRYds','WRTD','WPenYds','WSc%','WTO%','WOY/P','WFTO','WOPYds','WOPTD','WFInt','WORYds','WORTD','WDPenYds','WOSc%','WDTO%']
 Lkeys = ['LY/P','LTO','LPYds','LPTD','LInt','LRYds','LRTD','LPenYds','LSc%','LTO%','LOY/P','LFTO','LOPYds','LOPTD','LFInt','LORYds','LORTD','LDPenYds','LOSc%','LDTO%']
+reversedKeys = Lkeys + Wkeys
 
 # append each game from the season with the teams' stats
 dfGames = pd.concat([dfGames, pd.DataFrame(0, index=np.arange(len(dfGames)), columns=Wkeys)], axis=1)
@@ -98,28 +99,52 @@ for i in range(0,numGames):
 #              Code below is used for predicting the winning team              #
 ################################################################################
 
-# get list of winners
-y = dfGames.iloc[:,0]
+firstTeams = dfWeek16.iloc[:,0]
+secondTeams = dfWeek16.iloc[:,1]
+# get list of winning scores
+yW = dfGames.iloc[:,2]
+# get list of losing scores
+yL = dfGames.iloc[:,3]
 # get list of features to test
-X = dfGames.iloc[:,8:]
+Xw = dfGames.iloc[:,8:]
+Xl = Xw.ix[:, reversedKeys]
 
-predictX = dfWeek16.iloc[:,2:]
+predictXW = dfWeek16.iloc[:,2:]
+predictXL = predictXW.ix[:, reversedKeys]
 
-xTrain, xTest, yTrain, yTest = train_test_split(X, y, test_size=0.25, random_state=0)
+xwTrain, xwTest, ywTrain, ywTest = train_test_split(Xw, yW, test_size=0.25, random_state=0)
+xlTrain, xlTest, ylTrain, ylTest = train_test_split(Xl, yL, test_size=0.25, random_state=0)
 # logreg = LogisticRegression(C=1e5, solver='lbfgs', multi_class='multinomial')
-logreg = LogisticRegression()
-logreg.fit(xTrain, yTrain)
+logregW = LogisticRegression()
+logregL = LogisticRegression()
+
+logregW.fit(xwTrain, ywTrain)
+logregL.fit(xlTrain, ylTrain)
+
+predictionsW = logregW.predict(predictXW)
+predictionsL = logregL.predict(predictXL)
 
 # print("\nThe training set is:\n")
 # print(dfWeek16)
 
-predictions = logreg.predict(predictX)
+# predictions = logreg.predict(predictX)
 # print("\nThe real winners were:\n")
 # print(yTest)
-print("\nThe predicted winners were:\n")
-print("\n{}".format(predictions))
-score = logreg.score(xTest, yTest)
-print("\nAccuracy for predicting the winner was: {:.2f}%".format(score * 100))
+# print("\nThe predicted first team scores are:\n")
+# print("\n{}".format(predictionsW))
+# # score = logregW.score(xwTest, ywTest)
+# # print("\nAccuracy for predicting the winning score was: {:.2f}%".format(score * 100))
+#
+# print("\nThe predicted second team scores are:\n")
+# print("\n{}".format(predictionsL))
+# score = logregL.score(xlTest, ylTest)
+# print("\nAccuracy for predicting the secon score was: {:.2f}%".format(score * 100))
+
+for i in range(len(predictionsW)):
+    if predictionsW[i] > predictionsL[i]:
+        print("Winner of the {} game is {} by a score of {} to {}, line of {}".format(i + 1, firstTeams[i], predictionsW[i], predictionsL[i], predictionsW[i] - predictionsL[i]))
+    else:
+        print("Winner of the {} game is {} by a score of {} to {}, line of {}".format(i + 1, secondTeams[i], predictionsL[i], predictionsW[i], predictionsL[i] - predictionsW[i]))
 
 # ################################################################################
 # #              Code below is used for predicting the winning score             #
